cad1dff8db13906d127e343a552a8ed30bc1e04b
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-09-25 05:29:12 +0200

    Add project files.

---
+++ Prb.Variabelen01.WPF/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prb.Variabelen01.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string naam;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TxtUsername_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            naam = txtUsername.Text;
+            lblFeedback.Content = "Je naam is " + txtUsername.Text;
+        }
+
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+            lblHoofdletters.Content = naam.ToUpper();
+            MessageBox.Show("Ben jij werkelijk dé " + naam);
+        }
+    }
+}
